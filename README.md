d8bd55cf9a0518340323a9cc24ec6a2406db71b3
Rasmus T. Bjerg <rs.bjerg@gmail.com>
2022-09-04 23:22:37 +0200

    inital push

---
+++ README.md
@@ -0,0 +1,3 @@
+# sterm 
+
+(s)tupid (term)inal. Toggle between showing and hiding a neovim terminal buffer.
---
+++ lua/sterm/sterm.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+local split = "vsplit"
+
+M.setup = function(opts)
+	if opts.split_direction == "horizontal" then
+		split = "split"
+	end
+end
+
+local sterm_buf = -1
+
+M.toggle = function()
+	local buf_type = vim.api.nvim_buf_get_option(sterm_buf, "buftype")
+	local buf_valid = vim.api.nvim_buf_is_loaded(sterm_buf)
+	local sterm_exists = buf_type == "terminal" and buf_valid
+
+	if not sterm_exists then -- no sterm buffer exists -> create it and show it
+		vim.cmd(split)
+		local win = vim.api.nvim_get_current_win()
+		local buf = vim.api.nvim_create_buf(true, true)
+		vim.api.nvim_win_set_buf(win, buf)
+		vim.cmd("term")
+		sterm_buf = buf
+	else -- sterm buffer exists
+		local win_id = vim.fn.bufwinid(vim.fn.bufname(sterm_buf))
+		if win_id == -1 then -- sterm buffer is not visible -> open new win for it
+			vim.cmd(split)
+			local win = vim.api.nvim_get_current_win()
+			vim.api.nvim_win_set_buf(win, sterm_buf)
+		else -- sterm buffer is visible -> hide it
+			vim.api.nvim_win_hide(win_id)
+		end
+	end
+end
+
+return M
